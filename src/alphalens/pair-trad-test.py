783d6f3cc7af141cbb77b5ada01366b97909b3fc
bakuchi- <helveticaf0015se@gmail.com>
2021-02-08 20:27:49 +0900

    fix

--- src/alphalens/pair-trad-test.py
+++ src/alphalens/pair-trad-test.py
@@ -1,6 +1,8 @@
 import numpy as np
 
 # 在这个方法中编写任何的初始化逻辑。context对象将会在你的算法策略的任何方法之间做传递。
+
+
 def init(context):
     context.s1 = 'AG1612'
     context.s2 = 'AU1612'
@@ -62,7 +64,9 @@ def handle_bar(context, bar_dict):
         # 如果价差低于预先计算得到的下限,则为建仓信号,'买入'价差合约
         if spread <= down_limit and not context.down_cross_down_limit:
             # 可以通过logger打印日志
-            logger.info('spread: {}, mean: {}, down_limit: {}'.format(spread, mean, down_limit))
+            logger.info(
+                'spread: {}, mean: {}, down_limit: {}'.format(
+                    spread, mean, down_limit))
             logger.info('创建买入价差中...')
 
             # 获取当前剩余的应建仓的数量
@@ -81,7 +85,9 @@ def handle_bar(context, bar_dict):
 
         # 如果价差向上回归移动平均线,则为平仓信号
         if spread >= mean and context.down_cross_down_limit:
-            logger.info('spread: {}, mean: {}, down_limit: {}'.format(spread, mean, down_limit))
+            logger.info(
+                'spread: {}, mean: {}, down_limit: {}'.format(
+                    spread, mean, down_limit))
             logger.info('对买入价差仓位进行平仓操作中...')
 
             # 由于存在成交不超过下一bar成交量25%的限制,所以可能要通过多次发单成交才能够成功建仓
@@ -97,7 +103,9 @@ def handle_bar(context, bar_dict):
 
         # 如果价差高于预先计算得到的上限,则为建仓信号,'卖出'价差合约
         if spread >= up_limit and not context.up_cross_up_limit:
-            logger.info('spread: {}, mean: {}, up_limit: {}'.format(spread, mean, up_limit))
+            logger.info(
+                'spread: {}, mean: {}, up_limit: {}'.format(
+                    spread, mean, up_limit))
             logger.info('创建卖出价差中...')
             qty_a = 1 - short_pos_a.quantity
             qty_b = context.ratio - long_pos_b.quantity
@@ -111,7 +119,9 @@ def handle_bar(context, bar_dict):
 
         # 如果价差向下回归移动平均线,则为平仓信号
         if spread < mean and context.up_cross_up_limit:
-            logger.info('spread: {}, mean: {}, up_limit: {}'.format(spread, mean, up_limit))
+            logger.info(
+                'spread: {}, mean: {}, up_limit: {}'.format(
+                    spread, mean, up_limit))
             logger.info('对卖出价差仓位进行平仓操作中...')
             qty_a = short_pos_a.quantity
             qty_b = long_pos_b.quantity
